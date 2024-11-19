a064f41a39276b44af36ecfa1647807d9521a1a7
minhhoang609 <hellocung3@gmail.com>
2024-11-20 00:23:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ function makeChoice(choice) {
     let message;
     switch (choice) {
         case 1:
-            message = "Anh nho em";
+            message = "Anh nhớ em";
             break;
         case 2:
-            message = "Anh yeu em";
+            message = "Anh yêu em";
             break;
         case 3:
-            message = "Om 1 phat";
+            message = "Ôm 1 phát";
             break;
         case 4:
-            message = "Hun 1 cai";
+            message = "Hun 1 cái";
             break;
         case 5:
-            message = "Thom 1 mieng";
+            message = "Thơm 1 miếng";
             break;
         default:
             message = "Lua chon khong hop le. Ket thuc chuong trinh.";
